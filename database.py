c73c0d889cb0078ad7364d95e916a2678a2a6c3d
Box Chang <forkome@gmail.com>
2024-09-28 15:18:21 +0700

    Update

--- database.py
+++ database.py
@@ -2,8 +2,51 @@ import os
 import pyodbc
 from sqlite3 import Error
 
-
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+class mes_olap_database:
+    server = '192.168.11.31'
+    database = 'MES_OLAP'
+    username = 'vnedc'
+    password = 'vnedc#2024'
+    driver = 'ODBC Driver 17 for SQL Server'  # 根據你的環境選擇合適的 ODBC driver
+
+    def select_sql(self, sql):
+        self.conn = self.create_mes_olap_connection()
+        self.cur = self.conn.cursor()
+        self.cur.execute(sql)
+        return self.cur.fetchall()
+
+    def select_sql_dict(self, sql):
+        self.conn = self.create_mes_olap_connection()
+        self.cur = self.conn.cursor()
+        self.cur.execute(sql)
+
+        desc = self.cur.description
+        column_names = [col[0] for col in desc]
+        data = [dict(zip(column_names, row))
+                for row in self.cur.fetchall()]
+        return data
+
+    def execute_sql(self, sql):
+        self.conn = self.create_mes_olap_connection()
+        self.cur = self.conn.cursor()
+        self.cur.execute(sql)
+        self.conn.commit()
+
+    def create_mes_olap_connection(self):
+        try:
+            conn = pyodbc.connect("DRIVER={{SQL Server}};SERVER={server}; database={database}; \
+                                   trusted_connection=no;UID={uid};PWD={pwd}".format(server=self.server,
+                                                                                     database=self.database,
+                                                                                     uid=self.username,
+                                                                                     pwd=self.password))
+            return conn
+        except Error as e:
+            print(e)
+
+        return None
+
 
 class vnedc_database:
     def select_sql(self, sql):
