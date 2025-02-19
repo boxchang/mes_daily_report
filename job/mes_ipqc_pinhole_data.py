fc28666955c5cd1553ede643b8e29404bc370e4a
Box Chang <forkome@gmail.com>
2025-02-19 11:41:02 +0700

    Update

--- job/mes_ipqc_pinhole_data.py
+++ job/mes_ipqc_pinhole_data.py
@@ -8,7 +8,7 @@ from datetime import datetime, timedelta, date
 from database import vnedc_database, mes_database, mes_olap_database
 
 
-class ConvertMESJSON(object):
+class MES_IPQC_PINHOLE(object):
     date1 = ""
     date2 = ""
 
@@ -18,10 +18,26 @@ class ConvertMESJSON(object):
 
     def delete_data(self):
         mes_olap = mes_olap_database()
+        mes_db = mes_database()
+        tmp = ""
+        sql1 = f"""
+                SELECT id FROM PMG_MES_RunCard r where (((r.InspectionDate = '{self.date1}' AND r.Period BETWEEN 6 AND 23)
+                            OR (r.InspectionDate = DATEADD(DAY, 1, '{self.date2}') AND r.Period BETWEEN 0 AND 5))
+                            OR (r.InspectionDate between DATEADD(DAY, 1, '{self.date1}') AND '{self.date2}'))
+                """
+
+        raws = mes_db.select_sql_dict(sql1)
+        for raw in raws:
+            tmp += f"'{raw['id']}',"
+
+        if len(tmp) > 0:
+            tmp = tmp[:-1]
 
         sql = f"""
             delete from [MES_OLAP].[dbo].[mes_ipqc_pinhole_data] where 
-            create_at between CONVERT(DATETIME, '{self.date1} 00:00:00', 120) and CONVERT(DATETIME, '{self.date2} 23:59:59', 120)
+            runcard in (
+                {tmp}
+            )
         """
 
         mes_olap.execute_sql(sql)
@@ -36,8 +52,10 @@ class ConvertMESJSON(object):
 				  JOIN [PMGMES].[dbo].[PMG_MES_RunCard] r on r.Id = ipqc.RunCardId
 				  JOIN [PMGMES].[dbo].[PMG_MES_WorkOrder] w on r.WorkOrderId = w.Id
                   WHERE OptionName = 'Pinhole'
-                  AND ipqc.CreationTime between CONVERT(DATETIME, '{self.date1} 00:00:00', 120) and CONVERT(DATETIME, '{self.date2} 23:59:59', 120)
-                  AND SUBSTRING(JsonData,CHARINDEX(',',JsonData)-2,1) > 0
+                  AND (((r.InspectionDate = '{self.date1}' AND r.Period BETWEEN 6 AND 23)
+                    OR (r.InspectionDate = DATEADD(DAY, 1, '{self.date2}') AND r.Period BETWEEN 0 AND 5))
+                    OR (r.InspectionDate between DATEADD(DAY, 1, '{self.date1}') AND '{self.date2}'))
+                  AND TRY_CAST(SUBSTRING(JsonData,CHARINDEX(',',JsonData)-2,1) AS Int) > 0
                 """
         raws = mes_db.select_sql_dict(sql)
 
@@ -51,6 +69,7 @@ class ConvertMESJSON(object):
             customer_code = raw['CustomerCode']
             customer_name = raw['CustomerName']
             customer_partno = raw['CustomerPartNo']
+            values = ""
 
             for o in odata['Detail']:
                 try:
@@ -64,22 +83,24 @@ class ConvertMESJSON(object):
 
                     if len(results) > 0:
                         sql2 = f"""
-                                Update [MES_OLAP].[dbo].[mes_ipqc_pinhole_data] set qty = qty + {qty}
-                                Where runcard = '{runcard}' and defect_code = '{defect_code}'
+                                Update [MES_OLAP].[dbo].[mes_ipqc_pinhole_data] set qty = qty + ?
+                                Where runcard = ? and defect_code = ?
                         """
+                        values = (qty, runcard, defect_code)
                     else:
                         sql2 = f"""
                                 Insert Into [MES_OLAP].[dbo].[mes_ipqc_pinhole_data] (PartNo, ProductItem, CustomerCode, 
                                 CustomerName, CustomerPartNo, runcard, defect_code, qty, create_at, insert_at, cosmetic_inspect_qty)
-                                Values('{partno}','{product_item}','{customer_code}','{customer_name}','{customer_partno}','{runcard}', '{defect_code}', {qty}, '{create_time}', GETDATE(), {inspect_qty})
+                                Values(?,?,?,?,?,?,?,?,?, GETDATE(),?)
                                 """
+                        values = (partno, product_item, customer_code, customer_name, customer_partno, runcard, defect_code, qty, create_time, inspect_qty)
                     print(sql2)
-                    mes_olap.execute_sql(sql2)
+                    mes_olap.execute_sql_values(sql2, values)
                 except Exception as e:
                     print(e)
 
 
-    def execute(self):
+    def convert(self):
 
         self.delete_data()
         self.insert_data()
@@ -88,6 +109,5 @@ class ConvertMESJSON(object):
 report_date1 = datetime.today() - timedelta(days=1)
 report_date1 = report_date1.strftime('%Y%m%d')
 
-obj = ConvertMESJSON('20241001', '20250206')
-# obj = ConvertMESJSON(report_date1, report_date1)
-obj.execute()
+obj = MES_IPQC_PINHOLE(report_date1, report_date1)
+obj.convert()
